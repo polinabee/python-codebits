410219d044f47e7a5034ce36216b68c641724e82
Polina Berenstein <polinaberen@gmail.com>
2021-03-03 21:12:45 +0100

    adding dash insert

--- application/application.py
+++ application/application.py
@@ -2,13 +2,22 @@ def repeated_letter_count(str):
     max_reps_in_str = 0
     for word in str.split(' '):
         repeats_per_word = 0
-        for i in range(len(word)-1):
-            if word[i] == word[i+1]:
+        for i in range(len(word) - 1):
+            if word[i] == word[i + 1]:
                 repeats_per_word += 1
     max_reps_in_str = repeats_per_word if repeats_per_word > max_reps_in_str else max_reps_in_str
     return max_reps_in_str
 
 
+def dash_insert(str):
+    inserted_str = ''
+    for x in range(len(str) - 1):
+        inserted_str += str[x]
+        if int(str[x]) % 2 != 0 and int(str[x + 1]) % 2 != 0:
+            inserted_str += '-'
+    inserted_str += str[-1]
+    return inserted_str
+
+
 class Application:
     pass
-
--- tests/test_application.py
+++ tests/test_application.py
@@ -4,7 +4,11 @@ from application import application
 class MyTestCase(unittest.TestCase):
     def test_repeated_letter_count(self):
 
-        self.assertEqual(application.repeated_letter_count('repppeated leter cccount'), 2)
+        self.assertEqual(application.repeated_letter_count('reppeated leter cccount'), 2)
+
+    def test_dash_insert(self):
+
+        self.assertEqual(application.dash_insert('454793'),'4547-9-3')
 
 
 if __name__ == '__main__':
